78f610f6312e21a20210b72dba2109bae6922ae0
mundostr <idux.net@gmail.com>
2021-09-07 16:54:32 -0300

    Comentarios varios

--- sem3/ronda_personas.py
+++ sem3/ronda_personas.py
@@ -1,4 +1,11 @@
 # Primer versión ejercicio ronda personas
+"""
+Primer versión ejercicio ronda personas, con defectos de código pero cumpliendo con las funcionalidades solicitadas.
+El objetivo es que una ronda de 10 personas cuente hasta 100, uno a uno.
+1) Inicia en sentido horario
+2) Si la cuenta es perfectamente divisible por 8, la ronda debe cambiar de sentido.
+3) Si la cuenta es perfectamente divisible por 11, se debe mantener el sentido pero saltar la siguiente persona.
+"""
 
 LIMITE_CUENTA = 100
 LIMITE_PERSONAS = 10
@@ -11,8 +18,11 @@ sentido = "horario"
 
 for ciclo in range(LIMITE_CUENTA):
 	## Control de cuenta, persona
+	# Incrementamos la cuenta general
 	cuenta = cuenta + 1 # contador
 
+	# De acuerdo al sentido de giro actual, incrementamos o decrementamos el indicador de persona,
+	# y además verificamos que este indicador nunca se exceda de los límites de la ronda.
 	if (sentido == "horario"):
 		persona = persona + 1
 		if (persona > LIMITE_PERSONAS):
@@ -26,6 +36,7 @@ for ciclo in range(LIMITE_CUENTA):
 
 
 	# Control de perfectamente divisible por 8
+	# Si es perfectamente divisible por ocho, simplemente invertimos el sentido de giro
 	if (cuenta % 8 == 0):
 		if (sentido == "horario"):
 			sentido = "antihorario"
@@ -34,6 +45,8 @@ for ciclo in range(LIMITE_CUENTA):
 	
 
 	# Control de perfectamente divisible por 11
+	# Si es perfectamente divisible por 11, volvemos a controlar el sentido de giro
+	# para incrementar o decrementar otra vez el indicador de persona, y así saltar una.
 	if (cuenta % 11 == 0):
 		if (sentido == "horario"):
 			persona = persona + 1
