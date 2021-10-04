0ae1f18ca364c06071466e7776f11d1203c376dd
mundostr <idux.net@gmail.com>
2021-10-04 07:33:39 -0300

    Actualizaciones sem7

--- sem7/na_config.py
+++ sem7/na_config.py
@@ -10,3 +10,11 @@ load_dotenv()
 
 
 API_KEY = getenv("NEWSAPI_ID")
+
+# Declaraciones constantes y variables
+PAIS = "ar"
+CRITERIO = "tesla"
+FECHA = "2021-08-30"
+CATEGORIA = "sports"
+# URL_API = f"https://newsapi.org/v2/everything?q={CRITERIO}&from={FECHA}&sortBy=publishedAt&apiKey={cfg.API_KEY}"
+URL_API = f"https://newsapi.org/v2/top-headlines?country={PAIS}&category={CATEGORIA}&apiKey={API_KEY}"
--- sem7/newsapi.py
+++ sem7/newsapi.py
@@ -3,30 +3,28 @@ import requests
 import na_config as cfg
 
 
-# Declaraciones constantes y variables
-CRITERIO = "tesla"
-FECHA = "2021-08-29"
-URL_API = f"https://newsapi.org/v2/everything?q={CRITERIO}&from={FECHA}&sortBy=publishedAt&apiKey={cfg.API_KEY}"
-
-
 # Declaraciones Funciones
 def recuperar_contenido(direccion):
-	print(direccion)
-	solicitud = requests.get(direccion)
-
-	if (solicitud.status_code == 200):
-		return solicitud.json()
-	else:
+	try:
+		solicitud = requests.get(direccion)
+		
+		if (solicitud.status_code == 200):
+			return solicitud.json()
+		else:
+			print(solicitud.status_code)
+			return False
+	except requests.exceptions.RequestException as error:
+		print(error)
 		return False
-
-def main():
-	contenido = recuperar_contenido(URL_API)
-	if (contenido == False):
-		print("ERROR al recuperar contenidos desde newsapi")
-	else:
-		print(contenido)
 
 
 # Flujo principal
+def main():
+	contenido = recuperar_contenido(cfg.URL_API)
+	if (contenido == False):
+		print("ERROR general")
+	else:
+		print(contenido)
+
 if (__name__ == "__main__"):
 	main()
